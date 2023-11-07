da9d8b7425616c133a04e9820b5ee0d49ec53915
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-07 19:34:37 +0100

    Update 4.0

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,7 @@ const userInput = document.getElementById('validation-input');
 userInput.addEventListener("blur", (event) => {
     const eventValue = event.currentTarget.value.length;
     const requiredValue = userInput.dataset.length;
-    if ( eventValue === requiredValue) {
-        // userInput.style.borderColor = "#4caf50";
+    if ( eventValue == requiredValue) {
         userInput.classList.add('valid');
         userInput.classList.remove('invalid');
     }
